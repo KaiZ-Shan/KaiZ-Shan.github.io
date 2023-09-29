a14a17dc0cde9737af33dd0919a072b6da94c00c
凯Z闪 <78002438+KaiZ-Shan@users.noreply.github.com>
2023-09-29 11:30:19 +0800

    Update main.js

--- static/main.js
+++ static/main.js
@@ -80,11 +80,3 @@ $.ajax({
   },
 });
 
-fetch('v1.hitokoto.cn/?c=d')
-    .then(response => response.json())
-    .then(data => {
-      const hitokoto = document.getElementById('hitokoto_text')
-      hitokoto.href = 'https://hitokoto.cn/?uuid=' + data.uuid
-      hitokoto.innerText = data.hitokoto
-    })
-    .catch(console.error)
